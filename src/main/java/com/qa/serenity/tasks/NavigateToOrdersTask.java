149544dc2cbe9c8b34fa0c7a4b0b58c37e127179
MarianaCifuentes <marianacifuenteslondono@gmail.com>
2026-04-21 17:29:59 -0500

    Actualización de esperas al iniciar flujo

--- src/main/java/com/qa/serenity/tasks/NavigateToOrdersTask.java
+++ src/main/java/com/qa/serenity/tasks/NavigateToOrdersTask.java
@@ -3,8 +3,11 @@ package com.qa.serenity.tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static com.qa.serenity.ui.HomeUI.*;
+import static com.qa.serenity.ui.NewOrderUI.COSTUMER_SELECTION;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class NavigateToOrdersTask implements Task {
@@ -20,8 +23,11 @@ public class NavigateToOrdersTask implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
+                WaitUntil.the(STARTSHARP_MENU, WebElementStateMatchers.isVisible()).forNoMoreThan(10).seconds(),
                 Click.on(STARTSHARP_MENU),
+                WaitUntil.the(NORTHWIND_MENU, WebElementStateMatchers.isVisible()).forNoMoreThan(10).seconds(),
                 Click.on(NORTHWIND_MENU),
+                WaitUntil.the(ORDERS_OPTION, WebElementStateMatchers.isVisible()).forNoMoreThan(10).seconds(),
                 Click.on(ORDERS_OPTION)
         );
     }
